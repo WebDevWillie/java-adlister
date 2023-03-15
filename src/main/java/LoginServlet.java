8b7c99ec512ed90e817ca75c2f94a3c4bb09ce51
Will Parry <wsparry99@gmail.com>
2023-03-15 11:55:37 -0500

    .

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -8,15 +8,17 @@ import java.io.IOException;
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/login.jsp").forward(request, response);
+        request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        boolean validAttempt = username.equals("admin") && password.equals("password");
+        boolean validAttempt = username.length() > 1 && password.length() > 1;
 
         if (validAttempt) {
+            request.getSession().setAttribute("username", username);
+//            request.getSession().setAttribute("isLoggedIn", true);
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
--- src/main/java/ViewProfileServlet.java
+++ src/main/java/ViewProfileServlet.java
@@ -8,6 +8,13 @@ import java.io.IOException;
 @WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/profile.jsp").forward(request, response);
+
+        if(request.getSession().getAttribute("isLoggedIn") == null || !((boolean) request.getSession().getAttribute("isLoggedIn"))) {
+            response.sendRedirect("/login");
+        } else {
+            request.getRequestDispatcher("WEB-INF/profile.jsp").forward(request, response);
+        }
+
+
     }
 }
