32383bf8b74f240cd0bed07c433b7ff5c557b618
Will Parry <wsparry99@gmail.com>
2023-03-15 09:31:54 -0500

    .

---
+++ src/main/java/controllers/LoginServlet.java
@@ -0,0 +1,23 @@
+package controllers;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "login", urlPatterns = "/login")
+public class login extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getMethod().equalsIgnoreCase("post")) {
+            String username = request.getParameter("username");
+            String password = request.getParameter("password");
+            if (username.equals("admin") && password.equals("password")) {
+                response.sendRedirect("/profile");
+            }
+        }
+    }
+}
---
+++ src/main/java/models/Album.java
@@ -0,0 +1,70 @@
+
+
+public class Album  {
+    private int id;
+    private String artist;
+    private String name;
+    private int releaseDate;
+    private String genre;
+    private float sales;
+
+    public Album(int id, String artist, String name, int releaseDate, String genre, float sales) {
+        this.id = id;
+        this.artist = artist;
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.genre = genre;
+        this.sales = sales;
+    }
+
+    public Album() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(int releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public float getSales() {
+        return sales;
+    }
+
+    public void setSales(float sales) {
+        this.sales = sales;
+    }
+}
